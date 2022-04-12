1f8bf183998600e555ef12e48df8143042ca3092
KevTLW <kevinmcruzc@gmail.com>
2022-04-12 13:15:31 -0400

    add roulette route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
 import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import LandingPage from "./pages/LandingPage";
+import RoulettePage from "./pages/RoulettePage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/roulette" element={<RoulettePage />} />
       </Routes>
     </Router>
   );
