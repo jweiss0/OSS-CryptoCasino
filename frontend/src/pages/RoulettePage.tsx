50acc329ec347ae7ceaa015e4695342bc5e2a6b0
KevTLW <kevinmcruzc@gmail.com>
2022-04-21 18:19:48 -0400

    add very simple styling to roulette game

--- frontend/src/pages/RoulettePage.tsx
+++ frontend/src/pages/RoulettePage.tsx
@@ -63,6 +63,7 @@ interface Win {
   option: string;
   color: string;
 }
+
 export const RoulettePage = () => {
   const [mustStartSpinning, setMustStartSpinning] = useState(false);
   const [prizeNumber, setPrizeNumber] = useState(getRandomNumber());
@@ -238,6 +239,52 @@ export const RoulettePage = () => {
     }
   };
 
+  const removeBet = (value: string | number) => {
+    switch (value) {
+      case 0:
+        roulette.setZeros((p) => [0, p[1]]);
+        break;
+      case "00":
+        roulette.setZeros((p) => [p[0], 0]);
+        break;
+      case "odds":
+        roulette.setOddEven((p) => [0, p[1]]);
+        break;
+      case "evens":
+        roulette.setOddEven((p) => [p[0], 0]);
+        break;
+      case "reds":
+        roulette.setRedBlack((p) => [0, p[1]]);
+        break;
+      case "blacks":
+        roulette.setRedBlack((p) => [p[0], 0]);
+        break;
+      case "2nd 18":
+        roulette.setHighLow((p) => [0, p[1]]);
+        break;
+      case "1st 18":
+        roulette.setHighLow((p) => [p[0], 0]);
+        break;
+      case "1st 12":
+        roulette.setDozens((p) => [0, p[1], p[2]]);
+        break;
+      case "2nd 12":
+        roulette.setDozens((p) => [p[0], 0, p[2]]);
+        break;
+      case "3rd 12":
+        roulette.setDozens((p) => [p[0], p[1], 0]);
+        break;
+
+      default:
+        roulette.setStraightUps((p) => {
+          const straight = [...p];
+          straight[(value as number) - 1] = 0;
+          return straight;
+        });
+        break;
+    }
+  };
+
   const getBets = () => {
     const straights = roulette.straightUps
       .map((bets, index) => {
@@ -278,138 +325,316 @@ export const RoulettePage = () => {
 
   return (
     <div className="mt-2 w-100vw">
-      <h3 className="text-center">Last 20</h3>
-      <div className="d-flex justify-content-between mx-4">
-        <h6>recent</h6>
-        <h6>older</h6>
-      </div>
-      <ul className="" style={{ overflowX: "auto", width: "100vw" }}>
-        {[...previousWins].reverse().map((win) => (
-          <li
-            className="mr-2"
-            style={{
-              backgroundColor: win.color,
-              color: "white",
-              padding: "0.5rem",
-              borderRadius: "5px",
-              width: "40px",
-              textAlign: "center",
-              display: "inline",
-              marginRight: "1rem",
-              fontWeight: "bold",
-            }}
-            key={win.option}
-          >
-            {win.option}
-          </li>
-        ))}
-      </ul>
-      <div className="mx-auto" style={{ width: "fit-content" }}>
-        <Wheel
-          mustStartSpinning={mustStartSpinning}
-          prizeNumber={prizeNumber}
-          data={data}
-          innerRadius={35}
-          innerBorderWidth={25}
-          radiusLineColor="yellow"
-          radiusLineWidth={1}
-          textDistance={85}
-          outerBorderWidth={20}
-          textColors={["#ffffff"]}
-          spinDuration={0.01}
-          onStopSpinning={handleStopSpinning}
-        />
-        <div className="text-center mt-2">
-          <button className="btn btn-success" onClick={handleClick}>
-            spin
-          </button>
-          <button className="btn btn-danger ms-2" onClick={roulette.clear}>
-            clear bets
-          </button>
+      {previousWins.length !== 0 && (
+        <>
+          <h3 className="text-center">Last 20</h3>
+          <div className="d-flex justify-content-between mx-4">
+            <h6>recent</h6>
+            <h6>older</h6>
+          </div>
+          <ul className="w-100vw" style={{ overflowX: "auto" }}>
+            {[...previousWins].reverse().map((win) => (
+              <li
+                className="mr-2 old-result"
+                style={{
+                  backgroundColor: win.color,
+                }}
+                key={win.option}
+              >
+                {win.option}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+
+      <div className="d-flex align-items-center justify-content-between flex-column">
+        <div className="w-100">
+          <div className="w-100 overflow-auto">
+            <ul className="mt-2" style={{ width: "max-content" }}>
+              {getBets().zeros !== 0 && (
+                <li className="bet">
+                  {getBets().zeros} bet{getBets().zeros !== 1 && "s"} on zero{" "}
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => removeBet(0)}
+                  >
+                    remove bet
+                  </button>
+                </li>
+              )}
+              {getBets().doubleZeros !== 0 && (
+                <li className="bet">
+                  {getBets().doubleZeros} bet
+                  {getBets().doubleZeros !== 1 && "s"} on double zero{" "}
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => removeBet("00")}
+                  >
+                    remove bet
+                  </button>
+                </li>
+              )}
+              {getBets().odds !== 0 && (
+                <li className="bet">
+                  {getBets().odds} bet{getBets().odds !== 1 && "s"} on odds{" "}
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => removeBet("odds")}
+                  >
+                    remove bet
+                  </button>
+                </li>
+              )}
+              {getBets().evens !== 0 && (
+                <li className="bet">
+                  {getBets().evens} bet{getBets().evens !== 1 && "s"} on even{" "}
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => removeBet("evens")}
+                  >
+                    remove bet
+                  </button>
+                </li>
+              )}
+              {getBets().reds !== 0 && (
+                <li className="bet">
+                  {getBets().reds} bet{getBets().reds !== 1 && "s"} on red{" "}
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => removeBet("reds")}
+                  >
+                    remove bet
+                  </button>
+                </li>
+              )}
+              {getBets().blacks !== 0 && (
+                <li className="bet">
+                  {getBets().blacks} bet{getBets().blacks !== 1 && "s"} on black{" "}
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => removeBet("blacks")}
+                  >
+                    remove bet
+                  </button>
+                </li>
+              )}
+              {getBets().lows !== 0 && (
+                <li className="bet">
+                  {getBets().lows} bet{getBets().lows !== 1 && "s"} on 1 to 18{" "}
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => removeBet("1st 18")}
+                  >
+                    remove bet
+                  </button>
+                </li>
+              )}
+              {getBets().highs !== 0 && (
+                <li className="bet">
+                  {getBets().highs} bet{getBets().highs !== 1 && "s"} on 19 to
+                  36{" "}
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => removeBet("2nd 18")}
+                  >
+                    remove bet
+                  </button>
+                </li>
+              )}
+              {getBets().firstDozen !== 0 && (
+                <li className="bet">
+                  {getBets().firstDozen} bet{getBets().firstDozen !== 1 && "s"}{" "}
+                  on 1 to 12{" "}
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => removeBet("1st 12")}
+                  >
+                    remove bet
+                  </button>
+                </li>
+              )}
+              {getBets().secondDozen !== 0 && (
+                <li className="bet">
+                  {getBets().secondDozen} bet
+                  {getBets().secondDozen !== 1 && "s"} on 13 to 24{" "}
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => removeBet("2nd 12")}
+                  >
+                    remove bet
+                  </button>
+                </li>
+              )}
+              {getBets().thirdDozen !== 0 && (
+                <li className="bet">
+                  {getBets().thirdDozen} bet{getBets().thirdDozen !== 1 && "s"}{" "}
+                  on 25 to 36{" "}
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => removeBet("3rd 12")}
+                  >
+                    remove bet
+                  </button>
+                </li>
+              )}
+              {getBets().straights.map((s) => (
+                <li className="bet" key={s.value}>
+                  {s.bets} bet{s.bets !== 1 && "s"} on {s.value}{" "}
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => removeBet(s.value)}
+                  >
+                    remove bet
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="d-block mx-auto text-center">
+            <div className="d-flex align-items-center justify-content-center">
+              <Wheel
+                mustStartSpinning={mustStartSpinning}
+                prizeNumber={prizeNumber}
+                data={data}
+                innerRadius={35}
+                innerBorderWidth={25}
+                radiusLineColor="yellow"
+                radiusLineWidth={1}
+                textDistance={85}
+                outerBorderWidth={20}
+                textColors={["#ffffff"]}
+                spinDuration={0.01}
+                onStopSpinning={handleStopSpinning}
+              />
+            </div>
+            <div className="text-center mt-2">
+              <button className="btn btn-success" onClick={handleClick}>
+                spin
+              </button>
+              <button className="btn btn-danger ms-2" onClick={roulette.clear}>
+                clear bets
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-4">
+          <table className="mx-auto text-center">
+            <tbody>
+              <tr>
+                <td
+                  className="p-2 border border-warning bg-success text-white"
+                  onClick={() => handleClicky(0)}
+                >
+                  0
+                </td>
+                <td
+                  className="p-2 border border-warning bg-success text-white"
+                  onClick={() => handleClicky("00")}
+                >
+                  00
+                </td>
+              </tr>
+              <tr>
+                <td
+                  className="p-2 border border-warning bg-black text-white"
+                  onClick={() => handleClicky("odds")}
+                >
+                  odds
+                </td>
+                <td
+                  className="p-2 border border-warning bg-danger text-white"
+                  onClick={() => handleClicky("evens")}
+                >
+                  evens
+                </td>
+              </tr>
+              <tr>
+                <td
+                  className="p-2 border border-warning bg-danger text-white"
+                  onClick={() => handleClicky("reds")}
+                >
+                  reds
+                </td>
+                <td
+                  className="p-2 border border-warning bg-black text-white"
+                  onClick={() => handleClicky("blacks")}
+                >
+                  blacks
+                </td>
+              </tr>
+              <tr>
+                <td
+                  className="p-2 border border-warning border-bottom-0 bg-black text-white"
+                  onClick={() => handleClicky("1st 18")}
+                >
+                  1 to 18
+                </td>
+                <td
+                  className="p-2 border border-warning border-bottom-0 bg-danger text-white"
+                  onClick={() => handleClicky("2nd 18")}
+                >
+                  19 to 36
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <table className="mx-auto text-center">
+            <tbody>
+              <tr>
+                <td
+                  className="p-2 border border-warning bg-black text-white"
+                  onClick={() => handleClicky("1st 12")}
+                >
+                  1st 12
+                </td>
+                <td
+                  className="p-2 border border-warning bg-danger text-white"
+                  onClick={() => handleClicky("2nd 12")}
+                >
+                  2nd 12
+                </td>
+                <td
+                  className="p-2 border border-warning bg-black text-white"
+                  onClick={() => handleClicky("3rd 12")}
+                >
+                  3rd 12
+                </td>
+              </tr>
+              {Array.from({ length: 12 }, (_, i) => i).map((_, i) => (
+                <tr key={_}>
+                  <td
+                    className={`p-2 border border-warning text-white ${
+                      i % 2 === 0 ? "bg-danger" : "bg-black"
+                    }`}
+                    onClick={() => handleClicky(i * 3 + 1)}
+                  >
+                    {i * 3 + 1}
+                  </td>
+                  <td
+                    className={`p-2 border border-warning text-white ${
+                      (i + 1) % 2 === 0 ? "bg-danger" : "bg-black"
+                    }`}
+                    onClick={() => handleClicky(i * 3 + 2)}
+                  >
+                    {i * 3 + 2}
+                  </td>
+                  <td
+                    className={`p-2 border border-warning text-white ${
+                      (i + 2) % 2 === 0 ? "bg-danger" : "bg-black"
+                    }`}
+                    onClick={() => handleClicky(i * 3 + 3)}
+                  >
+                    {i * 3 + 3}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
-
-      <ul>
-        <li>
-          {getBets().zeros} bet{getBets().zeros !== 1 && "s"} on zero
-        </li>
-        <li>
-          {getBets().doubleZeros} bet{getBets().doubleZeros !== 1 && "s"} on
-          double zero
-        </li>
-        <li>
-          {getBets().odds} bet{getBets().odds !== 1 && "s"} on odds
-        </li>
-        <li>
-          {getBets().evens} bet{getBets().evens !== 1 && "s"} on even
-        </li>
-        <li>
-          {getBets().reds} bet{getBets().reds !== 1 && "s"} on red
-        </li>
-        <li>
-          {getBets().blacks} bet{getBets().blacks !== 1 && "s"} on black
-        </li>
-        <li>
-          {getBets().lows} bet{getBets().lows !== 1 && "s"} on 1 to 18
-        </li>
-        <li>
-          {getBets().highs} bet{getBets().highs !== 1 && "s"} on 19 to 36
-        </li>
-        <li>
-          {getBets().firstDozen} bet{getBets().firstDozen !== 1 && "s"} on 1 to
-          12
-        </li>
-        <li>
-          {getBets().secondDozen} bet{getBets().secondDozen !== 1 && "s"} on 13
-          to 24
-        </li>
-        <li>
-          {getBets().thirdDozen} bet{getBets().thirdDozen !== 1 && "s"} on 25 to
-          36
-        </li>
-        {getBets().straights.map((s) => (
-          <li key={s.value}>
-            {s.bets} bet{s.bets !== 1 && "s"} on {s.value}
-          </li>
-        ))}
-      </ul>
-
-      <table className="mx-auto text-center">
-        <tbody>
-          <tr>
-            <td onClick={() => handleClicky(0)}>0</td>
-            <td onClick={() => handleClicky("00")}>00</td>
-          </tr>
-          <tr>
-            <td onClick={() => handleClicky("odds")}>odds</td>
-            <td onClick={() => handleClicky("evens")}>evens</td>
-          </tr>
-          <tr>
-            <td onClick={() => handleClicky("reds")}>reds</td>
-            <td onClick={() => handleClicky("blacks")}>blacks</td>
-          </tr>
-          <tr>
-            <td onClick={() => handleClicky("1st 18")}>1 to 18</td>
-            <td onClick={() => handleClicky("2nd 18")}>19 to 36</td>
-          </tr>
-        </tbody>
-      </table>
-      <table className="mx-auto text-center">
-        <tbody>
-          <tr>
-            <td onClick={() => handleClicky("1st 12")}>1st 12</td>
-            <td onClick={() => handleClicky("2nd 12")}>2nd 12</td>
-            <td onClick={() => handleClicky("3rd 12")}>3rd 12</td>
-          </tr>
-          {Array.from({ length: 12 }, (_, i) => i).map((_, i) => (
-            <tr key={_}>
-              <td onClick={() => handleClicky(i * 3 + 1)}>{i * 3 + 1}</td>
-              <td onClick={() => handleClicky(i * 3 + 2)}>{i * 3 + 2}</td>
-              <td onClick={() => handleClicky(i * 3 + 3)}>{i * 3 + 3}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
     </div>
   );
 };
