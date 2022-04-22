ed5702a3bd11e29c7614620d637fffb331db0bea
Jeremy Weiss <jweiss045@gmail.com>
2022-04-22 14:48:33 -0400

    Add basic Roulette tests

--- test/index.ts
+++ test/index.ts
@@ -6,6 +6,7 @@ describe("Deployment", function () {
   let dChip: Contract;
   let dCasino: Contract;
   let dBlackjack: Contract;
+  let dRoulette: Contract;
 
   beforeEach(async () => {
     // Deploy Chip contract
@@ -18,8 +19,12 @@ describe("Deployment", function () {
     await dCasino.deployed();
     // Deploy Blackjack contract with minbet of 1, maxbet of 500, num decks 4
     const fBlackjack = await ethers.getContractFactory("Blackjack");
-    dBlackjack = await fBlackjack.deploy("1000000000000000000", "5000000000000000000000", 4); // minbet 1 in wei, maxbet 50 in wei
+    dBlackjack = await fBlackjack.deploy(ethers.utils.parseEther("1"), ethers.utils.parseEther("50"), 4); // minbet 1 in wei, maxbet 50 in wei
     await dBlackjack.deployed();
+    // Deploy Roulette contract with minbet of 1, maxbet of 500, num decks 4
+    const fRoulette = await ethers.getContractFactory("Roulette");
+    dRoulette = await fRoulette.deploy(ethers.utils.parseEther("1"), ethers.utils.parseEther("50")); // minbet 1 in wei, maxbet 50 in wei
+    await dRoulette.deployed();
   });
 
   it("Should successfully set initial contract values", async function () {
@@ -31,5 +36,8 @@ describe("Deployment", function () {
     // Set values in Blackjack (CasinoGame) contract
     await dBlackjack.setCasinoContractAddress(dCasino.address);
     await dBlackjack.setChipContractAddress(dChip.address);
+    // Set values in Roulette (CasinoGame) contract
+    await dRoulette.setCasinoContractAddress(dCasino.address);
+    await dRoulette.setChipContractAddress(dChip.address);
   });
 });
