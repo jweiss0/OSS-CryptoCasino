10cd61067f99861346c8de6d39df40cc02686b7f
eugroo <eugene.rozental@gmail.com>
2022-04-27 15:46:17 -0400

    add Settings page content

--- frontend/src/pages/slideInMenu.tsx
+++ frontend/src/pages/slideInMenu.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class SlideInMenu extends Component<any, any> {
     render() {
@@ -10,9 +11,29 @@ class SlideInMenu extends Component<any, any> {
 
         return (
             <div id="flyoutMenu"
-                onMouseDown={this.props.handleMouseDown}
+                //onMouseLeave={this.props.handleMouseDown}
                 className={visibility}>
-                <h3></h3>
+                <h3>Settings</h3>
+                <div className="settingsRow">
+                    <div className="settingsLeft">Theme:</div>
+                    <div className="settingsRight">
+                        <label className="switch">
+                            <input type="checkbox" id="togBtn"></input>
+                            <div className="slider round"></div>
+                        </label>
+                    </div>
+                </div>
+                <div className="settingsRow">
+                    <div className="settingsLeft">Metamask:</div>
+                    <Link to={{ pathname: "https://metamask.io/" }} target="_blank"><img className="settingsRight" src="MetaMask_Fox.png" height="50" width="50"></img></Link>
+                </div>
+                <div className="settingsRow">
+                    <div className="settingsLeft">About:</div>
+                </div>
+                <div className="contribute">
+                    <div className="settingsLeft">contribute:</div>
+                    <Link to={{ pathname: "https://github.com/jweiss0/OSS-CryptoCasino" }} target="_blank"><img className="settingsRight" src="githubIcon.png" height="30" width="30"></img></Link>
+                </div>
             </div>
         );
     }
